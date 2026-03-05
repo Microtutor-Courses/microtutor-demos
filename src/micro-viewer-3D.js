c5ca2b7add896d5ca59b85a62f9c7be0678b6e73
Eva <edelaserna94@gmail.com>
2026-03-05 15:50:06 -0500

    Fixed casing consistency of micro-viewer-3d component name

--- src/micro-viewer-3D.js
+++ src/micro-viewer-3D.js
@@ -703,16 +703,16 @@ update3DPlanes() {
 }
 
 // Register the custom element
-customElements.define('micro-viewer-3D', MicroViewer3D);
+customElements.define('micro-viewer-3d', MicroViewer3D);
 
 // For Moodle integration: Custom elemnts are sanitized or not allowed,
-// The code below is a work around: Look for divs with data-component="micro-viewer-3D"
+// The code below is a work around: Look for divs with data-component="micro-viewer-3d"
 // and replace them with the web component.
 function initViewers() {
 	document
-		.querySelectorAll('[data-component="micro-viewer-3D"]')
+		.querySelectorAll('[data-component="micro-viewer-3d"]')
 		.forEach((el) => {
-			const viewer = document.createElement("micro-viewer-3D");
+			const viewer = document.createElement("micro-viewer-3d");
 
 			// grab all data- attributes and pass them to the viewer
 			Object.keys(el.dataset).forEach((key) => {
